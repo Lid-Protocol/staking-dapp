6ffb4abac72238714a6e9f56181afb0c2bbbb115
carlsbad <carlsbad_sunshine@protonmail.com>
2020-08-01 21:06:33 -0700

    Upgrade to 0.1.5

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ export default function Header({web3, address, isWhitelisted, onConnect}) {
       </Flex>
       <Box color="lid.fgMed" display="block" w="100%"  pl={{base:"20px", lg:"0px"}} pr={{base:"20px", lg:"0px"}} maxW="1200px" ml="auto" mr="auto">
         <Text>
-          v0.1.4
+          v0.1.5
         </Text>
       </Box>
 
