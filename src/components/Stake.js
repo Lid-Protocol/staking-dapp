9a513cff0ef7843d33ceed5ffd16f3887da245b9
Jikun Liao <liaojikundever@gmail.com>
2020-09-23 22:02:56 -0500

    Fix decimal input on mobile

--- src/components/Stake.js
+++ src/components/Stake.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Text, Box, Button,NumberInput, NumberInputField,
+import { Text, Box, Button,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -58,77 +58,121 @@ export default function Stake({web3, address, lidStakingSC, accountLid, isRegist
     <Box w="100%" pt="20px" position="relative" textAlign="center">
       <Text fontSize="36px">Stake LID</Text>
       <Text color="lid.info" mt="10px" mb="0px">
-        Initial fee for staking registration is 400 LID.<br/>
+        Initial fee for staking registration is 400 LID.
+        <br />
         With a referral code, intitial fee is 200 LID.
       </Text>
       <Text color="#55595D" m="20px">
         Your registration fee will be
         <Text as="span" color="lid.fg">
-        {isRegistered ? (
-          " 0 "
-        ) : (
-          referralAddress !== "0x0000000000000000000000000000000000000000" ? (
-            " 200 "
-          ) : (
-            " 400 "
-          )
-        )
-        }
-         LID
-        </Text>.
+          {isRegistered
+            ? " 0 "
+            : referralAddress !== "0x0000000000000000000000000000000000000000"
+            ? " 200 "
+            : " 400 "}
+          LID
+        </Text>
+        .
       </Text>
-      <NumberInput display="block" w="500px" maxW="100%" m="30px" ml="auto" mr="auto"
+      <Input
+        display="block"
+        w="500px"
+        maxW="100%"
+        h="60px"
+        m="30px"
+        ml="auto"
+        mr="auto"
+        pl="30px"
+        type="number"
+        placeholder="Amount of LID to Stake"
+        borderRadius="30px"
+        
+        pattern="[0-9\.]*"
+        focusBorderColor="blue.500"
+        errorBorderColor="red.500"
+        isInvalid={
+          !!displayVal &&
+          (Number(displayVal) < 1 ||
+            Number(displayVal) > Number(fromWei(accountLid)))
+        }
         value={displayVal}
-        step="any"
-        min={1}
-        max={fromWei(accountLid)}
-      >
-        <NumberInputField type="number" placeholder="Amount of LID to Stake" h="60px" borderRadius="30px" pl="30px"
-          borderColor="lid.stroke"
-          focusBorderColor="blue.500"
-          errorBorderColor="red.500"
-          onChange={e => {
-              e.target.setAttribute("pattern","[0-9\.]*")
-              if(isNaN(e.target.value)) return
-              if(e.target.value === "") {
-                setDisplayVal("")
-              } else if(Number(e.target.value) > 140000000) {
-                setDisplayVal("140000000")
-              } else if(Number(e.target.value) < 0) {
-                setDisplayVal("0")
-              } else{
-                setDisplayVal(e.target.value)
-              }
-            }} />
-      </NumberInput>
-      <Button display="block" color="lid.bg" bg="lid.brand" h="60px" w="500px" maxW="100%" m="30px" borderRadius="30px" ml="auto" mr="auto"
+        onChange={(e) => {
+          const tempVal = e.target.value.replace(/[^\d.]/g, "");
+          if (tempVal === "") {
+            setDisplayVal("");
+          } else if (Number(tempVal) > 140000000) {
+            setDisplayVal("140000000");
+          } else if (Number(tempVal) < 0) {
+            setDisplayVal("0");
+          } else {
+            setDisplayVal(tempVal);
+          }
+        }}
+      />
+
+      <Button
+        display="block"
+        color="lid.bg"
+        bg="lid.brand"
+        h="60px"
+        w="500px"
+        maxW="100%"
+        m="30px"
+        borderRadius="30px"
+        ml="auto"
+        mr="auto"
         fontWeight="regular"
         onClick={handleStake}
       >
         Stake
-        </Button>
-      <Button display="block" color="lid.bg" bg="lid.buttonBgDk" h="60px" w="500px" maxW="100%" m="30px" borderRadius="30px" ml="auto" mr="auto"
+      </Button>
+      <Button
+        display="block"
+        color="lid.bg"
+        bg="lid.buttonBgDk"
+        h="60px"
+        w="500px"
+        maxW="100%"
+        m="30px"
+        borderRadius="30px"
+        ml="auto"
+        mr="auto"
         fontWeight="regular"
-        onClick={()=>setDisplayVal(fromWei(accountLid))}
+        onClick={() => setDisplayVal(fromWei(accountLid))}
       >
         Max
       </Button>
-      <Button variant="transparent" onClick={onOpen}>Enter Referral Code</Button>
+      <Button variant="transparent" onClick={onOpen}>
+        Enter Referral Code
+      </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose} >
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay zIndex="100" />
-        <ModalContent zIndex="101" p="20px" position="fixed" top="20px" left="50%" w="360px" ml="-180px" >
+        <ModalContent
+          zIndex="101"
+          p="20px"
+          position="fixed"
+          top="20px"
+          left="50%"
+          w="360px"
+          ml="-180px"
+        >
           <ModalHeader>Referral Code</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Text>Enter a referral code.</Text>
-            <Input type="text" onChange={e => {
-              if(!e.target.value || e.target.value.length !== 42 ) {
-                setReferralAddress("0x0000000000000000000000000000000000000000")
-              } else {
-                setReferralAddress(e.target.value)
-              }
-            }} />
+            <Input
+              type="text"
+              onChange={(e) => {
+                if (!e.target.value || e.target.value.length !== 42) {
+                  setReferralAddress(
+                    "0x0000000000000000000000000000000000000000"
+                  );
+                } else {
+                  setReferralAddress(e.target.value);
+                }
+              }}
+            />
           </ModalBody>
           <ModalFooter>
             <Button variant="transparent" onClick={onClose}>
@@ -137,12 +181,13 @@ export default function Stake({web3, address, lidStakingSC, accountLid, isRegist
           </ModalFooter>
         </ModalContent>
       </Modal>
-      <Text color="#55595D" >Referral Code : {(!referralAddress || referralAddress === "0x0000000000000000000000000000000000000000") ? (
-        "None"
-      ) : (
-        referralAddress.substring(0, 6) + "..." + referralAddress.slice(-4)
-      )
-    }</Text>
+      <Text color="#55595D">
+        Referral Code :{" "}
+        {!referralAddress ||
+        referralAddress === "0x0000000000000000000000000000000000000000"
+          ? "None"
+          : referralAddress.substring(0, 6) + "..." + referralAddress.slice(-4)}
+      </Text>
     </Box>
   );
 }
